e947989103e43e8f4cbea300d29f5b5f64642838
rauf <erk.rauf@gmail.com>
2019-01-25 14:36:35 +0300

    Adds index and style

---
+++ main.js
@@ -0,0 +1,43 @@
+function onLoad() {
+
+    // var box = document.querySelector('.box');
+    // var colorInput = document.getElementById('color');
+    // var changeColorButton = document.querySelector('.change-color');
+    // var styleInput = document.getElementById('style');
+    // var styleValue = document.getElementById('styleValue');
+    // changeColorButton.addEventListener('click', function () {
+    //     var color = colorInput.value;
+    //     box.style.backgroundColor = color;
+    // });
+            // var myStyle = styleInput.value;
+        // var myValue = styleValue.value;
+        // box.style[myStyle] = myValue;
+        // console.log(Screen);
+        // console.log(Screen.innerHTML);
+        // console.log(Screen.innerText);
+
+    let Screen = document.getElementsByClassName('item1')
+    console.log(Screen);
+
+
+
+    var Action = document.querySelector('actions');
+
+    Action.addEventListener('click', function () {
+        Screen = document.getElementById("Screen");
+
+        Screen.innerHTML = "ОБАНА!!!";
+        Screen.innerText = "ОБАНА!!!";
+        Screen.outerText = "ОБАНА!!!";
+        console.log(Screen);
+
+
+
+
+    });
+
+
+
+}
+
+
